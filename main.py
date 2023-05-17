588fec09e272e6a55a3e68ac1465afa65325465a
ali ibrahimli <alibrahimli041@gmail.com>
2023-05-17 16:07:48 +0400

    Final commit. in the future some changes can be

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 import glob
 import os
 from emailing import send_email
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 
@@ -51,8 +52,15 @@ while True:
     status_list = status_list[-2:]
     print(status_list)
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email(image_with_object)
-        clean_folder()
+        email_thread = Thread(target=send_email, args=(image_with_object, ))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_folder)
+        clean_thread.daemon = True
+
+        email_thread.start()
+        clean_thread.start()
+
+
 
     cv2.imshow("Video", frame)
 
