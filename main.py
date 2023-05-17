655bfe057fec92ba9521c228d1cd164f734585bf
ali ibrahimli <alibrahimli041@gmail.com>
2023-05-17 15:20:23 +0400

    Email sending configured

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
+import os
+
 import cv2
 import time
+import glob
+import os
 from emailing import send_email
+
 video = cv2.VideoCapture(0)
 
 first_frame = None
 status_list = []
+count = 1
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
 while True:
     status = 0
     time.sleep(0)
     check, frame = video.read()
+
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
 
@@ -17,27 +29,30 @@ while True:
 
     delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
 
-
     thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
-    cv2.imshow("My video", dil_frame)
-
 
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
-        if cv2.contourArea(contour) <7000:
+        if cv2.contourArea(contour) < 7000:
             continue
-        x, y, w, h =cv2.boundingRect(contour)
-        rectangle = cv2.rectangle(frame, (x,y),(x+w, y+h), (0, 255, 0), 2)
+        x, y, w, h = cv2.boundingRect(contour)
+        rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
     print(status_list)
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(image_with_object)
+        clean_folder()
 
     cv2.imshow("Video", frame)
 
